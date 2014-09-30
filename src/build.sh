8b9aed8bd1c679e1d5b4464837e33db5b70d3a01
Marcel Doumen <marcel.doumen@golivedocs.com.br>
2014-09-30 19:34:03 -0300

    Power Build Script

---
+++ src/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+for file in `find ../ -name "*.jar"`
+do
+  THE_CLASSPATH=${THE_CLASSPATH}:${file}
+done
+
+for file in `find  $JBOSS_HOME/modules/system/layers/base -name "*.jar"`
+do
+  THE_CLASSPATH=${THE_CLASSPATH}:${file}
+done
+
+echo $THE_CLASSPATH
+
+mkdir -p ../WebContent/WEB-INF/classes
+cp -r * ../WebContent/WEB-INF/classes
+find ../WebContent/WEB-INF/classes -name "*.java" -exec javac -cp ".:${THE_CLASSPATH}" {} \;
+find ../WebContent/WEB-INF/classes -name "*.java" -exec rm -rf {} \;
+find ../WebContent/WEB-INF/classes -name "*~" -exec rm -rf {} \;
+find ../WebContent/WEB-INF/classes -name "deploy.sh" -exec rm -f {} \;
+
+
